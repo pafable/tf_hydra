b27f815d2a9f7ec73badf853c530f7189863fe86
pafable <pafable@gmail.com>
2020-05-23 18:49:42 -0400

    changed startup script for ubuntu

--- startup.tf
+++ startup.tf
@@ -1,4 +1,4 @@
 resource "vultr_startup_script" "my_startup" {
-    name = "init.sh"
+    name = "tf_hydra_init.sh"
     script = "apt update -y && apt upgrade -y && apt install wget net-tools vim -y ; echo 'sup yo!'>> /tmp/hello.txt"
 }
